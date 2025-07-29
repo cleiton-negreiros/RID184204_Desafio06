320e0ce6d1a928d666b2b4b399a4e234374c944f
cleiton-negreiros <cleiton.negreiros@gmail.com>
2025-07-29 18:35:13 -0300

    Atualiza scripts e melhorias no projeto

--- start-mongodb.bat
+++ start-mongodb.bat
@@ -1,4 +1,17 @@
 @echo off
 echo Iniciando MongoDB...
+
+REM Garante que a pasta de dados existe
+if not exist "C:\data\db" (
+    mkdir "C:\data\db"
+)
+
+REM Verifica se já existe um mongod rodando
+tasklist | findstr mongod.exe > nul
+if %errorlevel%==0 (
+    echo O MongoDB já está rodando.
+    exit /b
+)
+
 cd "C:\Program Files\MongoDB\Server\8.0\bin"
 mongod.exe --dbpath "C:\data\db"
